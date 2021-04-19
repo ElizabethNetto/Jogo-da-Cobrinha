df4b0bb1cdbf55d782cac7fa4ca8485678eb6c69
ElizabethNetto <elizamjnetto@gmail.com>
2021-04-19 17:20:27 -0300

    Jogo pronto

--- script.js
+++ script.js
@@ -13,22 +13,24 @@ let food = {
 }
 
 function criarBG() {
-    context.fillStyle = "lightgreen";
-    context.fillrect(0,0,16 * box, 16 * box);
+    context.fillStyle = "black";
+    context.fillRect(0,0,16 * box, 16 * box);
 }
 
 function criarCobrinha(){
     for(i=0; i < snake.length; i++){
         context.fillStyle = "green";
-        context.fillrect(snake[i].x, snake[i].y, box, box);
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
 
 function drawFood(){
     context.fillStyle = "red";
-    context.fillrect(food.x, food.y, box, box);
+    context.fillRect(food.x, food.y, box, box);
 }
+
+
 document.addEventListener('keydown', update);
 
 function update (event){
@@ -41,10 +43,18 @@ function update (event){
 
 
 function iniciarJogo(){
+
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
 
     criarBG();
     criarCobrinha();
@@ -61,10 +71,11 @@ function iniciarJogo(){
     if(snakeX != food.x || snakeY != food.y){
         snake.pop();
     }
-    else{food.x = Math.floor(Math.random() * 15 + 1) * box;
+    else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
         food.y = Math.floor(Math.random() * 15 + 1) * box;
     }
-        
+
     let newHead = {
         x: snakeX,
         y: snakeY
